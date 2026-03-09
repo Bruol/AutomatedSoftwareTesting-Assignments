ee399c8d7e2fb9de54da8255475260d222ba75d8
bruol <lorin@urbantat.eu>
2026-03-09 17:25:33 +0100

    finish toy2

--- hw1/toy2/test2.cpp
+++ hw1/toy2/test2.cpp
@@ -1,26 +1,42 @@
 #include "LinkedList.h"
-#include <iostream>
 #include <functional>
+#include <iostream>
+#include <stdexcept>
 
 bool testcase0() {
-    LinkedList list;
-    list.push_back(5);
-
-    return list.size() == 1;
+  LinkedList list;
+  list.push_back(5);
+  list.push_back(5);
+  list.push_front(5);
+  list.push_front(5);
+  list[1];
+  list[2];
+  list.erase(4);
+  list.erase(3);
+  list.erase(0);
+  list.erase(0);
+  list.erase(0);
+  list.push_front(5);
+  list.size();
+  try {
+    list[1];
+  } catch (const std::out_of_range &e) {
+    std::cout << "Caught exception: " << e.what() << std::endl;
+  }
+  return true;
 }
 
 // TODO add test cases to achieve 100% line coverage in LinkedList.cpp
 std::function<bool()> tests[] = {
-        testcase0
+    testcase0,
 };
 
 int main() {
-    unsigned ran = 0, passed = 0;
-    for (auto test : tests) {
-        ran++;
-        passed += test();
-    }
-    std::cout << "Passed " << passed << "/" << ran << " test cases!" << std::endl;
-    return 0;
+  unsigned ran = 0, passed = 0;
+  for (auto test : tests) {
+    ran++;
+    passed += test();
+  }
+  std::cout << "Passed " << passed << "/" << ran << " test cases!" << std::endl;
+  return 0;
 }
-

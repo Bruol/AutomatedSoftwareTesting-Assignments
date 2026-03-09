e4cff0a17235aff863ed8b8de6633c08c53dbb37
bruol <lorin@urbantat.eu>
2026-03-09 17:59:04 +0100

    toy3 done

--- hw1/toy3/test3.cpp
+++ hw1/toy3/test3.cpp
@@ -1,27 +1,79 @@
 #include "BinaryTree.h"
-#include <iostream>
 #include <functional>
+#include <iostream>
 
 bool testcase0() {
-    BinaryTree tree;
+  BinaryTree tree;
 
-    tree.add(0);
+  tree.add(0);
+  tree.add(1);
+  tree.add(2);
+  tree.add(3);
+  tree.add(4);
+  tree.add(5);
+  tree.add(-1);
+  tree.add(-2);
+  tree.add(-3);
+  tree.add(-4);
 
-    return true;
+  tree.remove(-2);
+  tree.remove(2);
+  tree.remove(1);
+  tree.remove(5);
+
+  return true;
+}
+bool testcase1() {
+  BinaryTree tree;
+
+  tree.add(0);
+  tree.add(2);
+  tree.add(1);
+  tree.add(4);
+  tree.add(3);
+  tree.add(5);
+  tree.add(-3);
+  tree.add(-2);
+  tree.add(-1);
+  tree.add(-4);
+
+  tree.remove(0);
+  tree.remove(-2);
+  tree.remove(2);
+  tree.remove(1);
+  tree.remove(5);
+
+  return true;
+}
+
+bool testcase2() {
+  BinaryTree tree;
+
+  tree.add(0);
+
+  tree.add(-3);
+  tree.add(-2);
+  tree.add(-1);
+  tree.add(-4);
+
+  tree.remove(0);
+  tree.remove(-2);
+  tree.remove(2);
+  tree.remove(1);
+  tree.remove(5);
+
+  return true;
 }
 
 // TODO add test cases to achieve 100% line coverage in BinaryTree.cpp
-std::function<bool()> tests[] = {
-        testcase0
-};
+std::function<bool()> tests[] = {testcase0, testcase1, testcase2};
 
 int main() {
-    unsigned ran = 0, passed = 0;
-    for (auto test : tests) {
-        ran++;
-        passed += test();
-    }
-    std::cout << "Passed " << passed << "/" << ran << " test cases!" << std::endl;
-    return 0;
+  unsigned ran = 0, passed = 0;
+  for (auto test : tests) {
+    ran++;
+    passed += test();
+  }
+  std::cout << "Passed " << passed << "/" << ran << " test cases!" << std::endl;
+  return 0;
 }
-
